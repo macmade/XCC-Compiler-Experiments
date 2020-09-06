ff9c180201684762feeecf68c3581c4189377c09
macmade <macmade@xs-labs.com>
2020-09-06 13:38:04 +0200

    Holub 1.7

---
+++ projects/holub/1.7/Lexer.h
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @header      Lexer.h
+ * @copyright   (c) 2020, Jean-David Gadina - www.xs-labs.com
+ * @dicussion   Adapted from "Compiler Design in C" by Allen I. Holub.
+ *              ISBN 0-13-155045-4 - https://holub.com/compiler
+ */
+
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum
+{
+    TokenEnd              = 0,  /* End of input */
+    TokenSemicolon        = 1,  /* ; */
+    TokenAdd              = 2,  /* + */
+    TokenMultiply         = 3,  /* * */
+    TokenLeftParenthesis  = 4,  /* ( */
+    TokenRightParenthesis = 5,  /* ) */
+    TokenNumericOrID      = 6   /* Decimal number or identifier */
+}
+Token;
+
+const char * Lexer_GetText( void );
+size_t       Lexer_GetLength( void );
+size_t       Lexer_GetLine( void );
+
+Token Lexer_Next( void );
+void  Lexer_Advance( void );
+bool  Lexer_Match( Token token );
+bool  Lexer_LegalLookahead( Token first, ... );
+
+#endif /* LEXER_H */
---
+++ projects/holub/1.7/Parser.c
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        Parser.c
+ * @copyright   (c) 2020, Jean-David Gadina - www.xs-labs.com
+ * @dicussion   Adapted from "Compiler Design in C" by Allen I. Holub.
+ *              ISBN 0-13-155045-4 - https://holub.com/compiler
+ */
+
+#include "Parser.h"
+#include "Lexer.h"
+#include "Print.h"
+#include <stdio.h>
+
+/*
+ * statements -> expression SEMICOLON | expression SEMI statements
+ */
+void Parser_Statements( void )
+{
+    while( Lexer_Match( TokenEnd ) == false )
+    {
+        Parser_Expression();
+        
+        if( Lexer_Match( TokenSemicolon ) )
+        {
+            Lexer_Advance();
+        }
+        else
+        {
+            Warning( "Inserting missing semicolon" );
+        }
+    }
+}
+
+/*
+ * expression  -> term expression'
+ * expression' -> ADD term expression' | epsilon
+ */
+void Parser_Expression( void )
+{
+    if( Lexer_LegalLookahead( TokenNumericOrID, TokenLeftParenthesis, TokenEnd ) == false )
+    {
+        return;
+    }
+    
+    Parser_Term();
+    
+    while( Lexer_Match( TokenAdd ) )
+    {
+        Lexer_Advance();
+        Parser_Term();
+    }
+}
+
+/*
+ * term  -> factor term'
+ * term' -> MULTIPLY factor term' | epsilon
+ */
+void Parser_Term( void )
+{
+    if( Lexer_LegalLookahead( TokenNumericOrID, TokenLeftParenthesis, TokenEnd ) == false )
+    {
+        return;
+    }
+    
+    Parser_Factor();
+    
+    while( Lexer_Match( TokenMultiply ) )
+    {
+        Lexer_Advance();
+        Parser_Factor();
+    }
+}
+
+/* factor -> NUMERIC_OR_ID | LEFT_PARENTHESIS expression RIGHT_PARENTHESIS */
+void Parser_Factor( void )
+{
+    if( Lexer_LegalLookahead( TokenNumericOrID, TokenLeftParenthesis, TokenEnd ) == false )
+    {
+        return;
+    }
+    
+    if( Lexer_Match( TokenNumericOrID ) )
+    {
+        Lexer_Advance();
+    }
+    else if( Lexer_Match( TokenLeftParenthesis ) )
+    {
+        Lexer_Advance();
+        Parser_Expression();
+        
+        if( Lexer_Match( TokenRightParenthesis ) )
+        {
+            Lexer_Advance();
+        }
+        else
+        {
+            Error( "Mismatch parenthesis" );
+        }
+    }
+    else
+    {
+        Error( "Number or identifier expected" );
+    }
+}
